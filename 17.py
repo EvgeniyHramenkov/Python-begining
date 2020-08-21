8bcf7b6c18c9d694f64670cb6cdcf3d2b42cf7ec
EvgeniyHramenkov <69689343+EvgeniyHramenkov@users.noreply.github.com>
2020-08-21 22:16:08 +0300

    Add files via upload

---
+++ 17.py
@@ -0,0 +1,15 @@
+# по умолчанию файл открывается для чтения, иначе нужно вбивать ключ для 
+# - вида работы с файлом
+#with open('1.txt', 'w') as document: 
+	#for line in document:
+		#print(line)
+
+with open('img.bmp', 'rb') as image:	
+	image.write()	
+
+# чтобы запустить скрипт нужно перейти в папку с етим скриптом
+# cd Doc
+# cd c:\py
+print('Hello world!')	
+
+	
